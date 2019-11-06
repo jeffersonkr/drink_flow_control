39af190c0c2e73190267aec9fb5844526ab44e9f
Jefferson <jeffersonkr@hotmail.com>
2019-11-06 19:08:05 -0300

    setting database mysql

--- drink_flow_control/settings.py
+++ drink_flow_control/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%gw791opzx+6nnyam=x%26h+ds8tg)x#pes+zxa$9$oxlb_6!v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'drink_flow_control.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'drink_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
