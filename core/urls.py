39343e55a2c663944f14baa3a6306ad2c47fa869
Jefferson <jeffersonkr@hotmail.com>
2019-11-07 15:16:22 -0300

    add take_photo, edit urls

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from core.views import user, login, cadastro
+from core.views import user, login, cadastro, edit, take_photo
 
 urlpatterns = [
     path('', login, name='login'),
     path('user/<int:user_id>', user, name='user'),
     path('cadastro/<str:code_number>', cadastro, name='cadastro'),
+    path('edit/<str:code_number>', edit, name='edit'),
+    path('take_photo/<str:code_number>', take_photo, name='take_photo'),
 ]
