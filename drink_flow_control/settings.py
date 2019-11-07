7a1cd87294b0e82ab48e426613f795291f35cfb6
jefferson kwak <jeffersonkr@hotmail.com>
2019-11-07 01:34:15 -0300

    add media url and root

--- drink_flow_control/settings.py
+++ drink_flow_control/settings.py
@@ -123,4 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
