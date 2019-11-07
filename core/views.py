8525d3a8b1529c0fd560e49414fc2d278c70ede6
jefferson kwak <jeffersonkr@hotmail.com>
2019-11-07 01:01:34 -0300

    add cadastro func

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from .forms.user_form import UserForm
+from .models.user import User
 
 # Create your views here.
 
@@ -11,15 +13,33 @@ def login(request):
         quarto_digito = request.POST.get('quarto_digito')
 
         numero = map(str, [primeiro_digito, segundo_digito, terceiro_digito, quarto_digito])
-        codigo = "".join(list(numero))
-        if codigo == '1234':
+        code_number = "".join(list(numero))
+        if code_number == '1234':
             return redirect('user')
         else:
-            return redirect('cadastro')
+            return redirect('cadastro', code_number)
         
     return render(request, 'login.html')
 
-def cadastro(request):
+def cadastro(request, code_number):
+    if request.POST:
+        form = UserForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            cellphone = form.cleaned_data['cellphone']
+            User.objects.create(name=name, cellphone=cellphone).save()
+            
+            return redirect('login')
+
+        else:
+            context = {
+                'error_nome': form.errors.get("nome", ""),
+                'error_cel': form.errors.get("cellphone", "")
+                }
+
+            return render(request, 'register.html', context)
+
+
     return render(request, 'register.html')
 
 def user(request):
