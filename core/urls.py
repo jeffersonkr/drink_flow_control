e7805b8410efba0c7ae371cd25f2fbd088d328ca
jefferson kwak <jeffersonkr@hotmail.com>
2019-11-07 00:02:44 -0300

    getting code number from url

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from core.views import user, login, cadastro
 urlpatterns = [
     path('', login, name='login'),
     path('user/', user, name='user'),
-    path('cadastro/', cadastro, name='cadastro'),
+    path('cadastro/<str:code_number>', cadastro, name='cadastro'),
 ]
