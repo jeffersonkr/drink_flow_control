1978e55b8b372bcea72c78c805bd7fe0dc08f2a1
jefferson kwak <jeffersonkr@hotmail.com>
2019-11-07 22:45:06 -0300

    add alert after solenoide close

--- core/static/js/timer.js
+++ core/static/js/timer.js
@@ -1,21 +1,30 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 $('#set_clock_300').on('click', function(){
     console.log('300ml');
-    setTimeout(function(){
+    setTimeout(async function(){
         home = (
             window.location.protocol + 
             "//" + window.location.host + 
             "/" + (window.location.href).split("/")[1]);
+        $(".alert").show().fadeTo(3500, 0);
+        await sleep(2500);
         window.location.replace(home);
     }, 10000);
+    
  });
 
 $('#set_clock_1000').on('click', function(){
     console.log('1000ml');
-    setTimeout(function(){
+    setTimeout(async function(){
         home = (
             window.location.protocol + 
             "//" + window.location.host + 
             "/" + (window.location.href).split("/")[1]);
+        $(".alert").show().fadeTo(3500, 0);
+        await sleep(2500);
         window.location.replace(home);
     }, 5000*3.3);
  });
