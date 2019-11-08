afd8858aa61ee011fd5f43deb392c5708fa190c7
Jefferson <jeffersonkr@hotmail.com>
2019-11-08 17:32:04 -0300

    add strict mode to bd

--- drink_flow_control/settings.py
+++ drink_flow_control/settings.py
@@ -83,6 +83,9 @@ DATABASES = {
         'PASSWORD': 'drinkwater',
         'HOST': 'db', 
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
