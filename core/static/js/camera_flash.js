133bbe8ca45f511eb5eb28b2958c2148eec0a61f
jefferson kwak <jeffersonkr@hotmail.com>
2019-11-09 02:06:37 -0300

    remove camera flash , didnt worked

--- core/static/js/camera_flash.js
+++
@@ -1,8 +0,0 @@
-$(".li_camera").on("click", function(){
-    $('#shutter').addClass('on');
-    setTimeout(function() {
-        $('#shutter').removeClass('on');
-    }, 30*2+45);/* Shutter speed (double & add 45) */
-})
-
-
