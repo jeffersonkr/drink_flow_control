10bc55f12ce3df04c566a179f8109e94b5ab3146
jefferson kwak <jeffersonkr@hotmail.com>
2019-11-09 01:31:40 -0300

    add windows shot

--- core/static/js/camera_flash.js
+++ core/static/js/camera_flash.js
@@ -1,6 +1,6 @@
 $(".li_camera").on("click", function(){
-    this.css("background-color","#FF0000");
+    $(window).css("background-color","#FF0000");
     setTimeout(function(){
-        this.css("background-color","#FFFFFF"); 
+        $(window).css("background-color","#FFFFFF"); 
     },800);
 })
