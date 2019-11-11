4758e94d25b51c6802112158f6a9e49226740583
Jefferson <jeffersonkr@hotmail.com>
2019-11-11 15:25:37 -0300

    add teste

---
+++ core/utils/teste_vazao.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+FLOW_SENSOR = 40
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+global count
+count = 0
+
+def count_pulse(channel):
+   global count
+   count = count+1
+   print(count)
+
+
+GPIO.add_event_detect(FLOW_SENSOR, GPIO.RISING, callback=countPulse)
+
+while True:
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print '\ncaught keyboard interrupt!, bye'
+        GPIO.cleanup()
+        sys.exit()
