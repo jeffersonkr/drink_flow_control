cc752f3368977331f48a8cf86d16cdb4d05c1ddb
Jefferson <jeffersonkr@hotmail.com>
2019-11-11 15:57:11 -0300

    print counter per sec

--- core/utils/teste_vazao.py
+++ core/utils/teste_vazao.py
@@ -1,27 +1,31 @@
 import RPi.GPIO as GPIO
 import time
 import sys
+from datetime import datetime
 
+count = 0
+start = datetime.now()
+count_per_sec = 0
 FLOW_SENSOR = 40
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-global count
-count = 0
+GPIO.add_event_detect(FLOW_SENSOR, GPIO.RISING, callback=count_pulse)
 
 def count_pulse(channel):
-    start = time.time()
-    global count
+    global count, start, count_per_sec
     count = count+1
-    end = time.time()-start
-    print(count, end)
+    if datetime.now() - start == 1:
+        count_per_sec += count
+        print(count_per_sec)
 
 
-GPIO.add_event_detect(FLOW_SENSOR, GPIO.RISING, callback=count_pulse)
 while True:
     try:
         time.sleep(1)
     except KeyboardInterrupt:
+        end = datetime.now()
         print('\ncaught keyboard interrupt!, bye')
+        total_time = end - start
+        frequency = count/total_time
         GPIO.cleanup()
         sys.exit()
