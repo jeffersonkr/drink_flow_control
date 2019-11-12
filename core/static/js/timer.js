253db99dd4de247f3f752d0b43b20eeadccef7c0
Jefferson <jeffersonkr@hotmail.com>
2019-11-12 17:36:54 -0300

    fix timer

--- core/static/js/timer.js
+++ core/static/js/timer.js
@@ -1,12 +1,9 @@
 document.querySelector('#set_clock_300').onclick = function(e){
-    let user_id = $("#user_id").text();
-    $.get("/user/" + user_id + "/300", function(response) {
-        console.log(response);
-    });
+    let url = location.href;
+    location.replace(url + '/300');
 };
 
 document.querySelector('#set_clock_1000').onclick = function(e){
-    console.log("window.location.href");
     let url = location.href;
     location.replace(url + '/1000');
 };
