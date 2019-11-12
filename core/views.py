47c64f9e35ce7b05d4e65a58f6ce50eecde562ff
Jefferson <jeffersonkr@hotmail.com>
2019-11-12 16:53:19 -0300

    script changed to full path

--- core/views.py
+++ core/views.py
@@ -109,7 +109,7 @@ def user(request, user_id):
         'user_updated': user.updated_at,
         'user_code_number': user.code_number,
         'user_photo': user.photo if user.photo else None,
-        'script': "{% static 'js/timer.js' %}",
+        'script': "/static/js/timer.js",
     }
 
     return render(request, 'user.html', context)
@@ -127,7 +127,7 @@ def start_monitoring(request, user_id, qtd_water):
         'user_updated': user.updated_at,
         'user_code_number': user.code_number,
         'user_photo': user.photo if user.photo else None,
-        'script': "{% static 'js/get_monitoring.js' %}",
+        'script': "/static/js/get_monitoring.js",
     }
 
     return render(request, 'user.html', context)
