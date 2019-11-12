773c82babfc4d760e59b2ffeb5f5cba773cc7e21
Jefferson <jeffersonkr@hotmail.com>
2019-11-12 14:19:20 -0300

    add path for start monitoring flow

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from core.views import user, login, cadastro, edit
+from core.views import user, login, cadastro, edit, start_monitoring
 
 urlpatterns = [
     path('', login, name='login'),
     path('user/<int:user_id>', user, name='user'),
     path('cadastro/<str:code_number>', cadastro, name='cadastro'),
     path('edit/<str:code_number>', edit, name='edit'),
+    path('user/<int:user_id>/OK', start_monitoring, name='monitoring')
 ]
