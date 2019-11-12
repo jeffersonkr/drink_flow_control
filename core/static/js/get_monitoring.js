c4f838a4a3461552d6a050e27faf47bc651e9ab0
Jefferson <jeffersonkr@hotmail.com>
2019-11-12 17:49:54 -0300

    window on load send request to ws

--- core/static/js/get_monitoring.js
+++ core/static/js/get_monitoring.js
@@ -4,31 +4,17 @@ var chatSocket = new WebSocket(
 chatSocket.onmessage = function(e) {
     var data = JSON.parse(e.data);
     var message = JSON.parse(data['message']);
-    document.querySelector('#qtd_liquido').textContent = message[''];
-    document.querySelector('#qtd_faltante').textContent = total - qtd_liquido
-    document.querySelector('#qtd_total_diario').textContent = total
+    document.querySelector('#qtd_liquido').textContent = message;
+    //document.querySelector('#qtd_faltante').textContent = total - qtd_liquido
+    //document.querySelector('#qtd_total_diario').textContent = total
 };
 
 chatSocket.onclose = function(e) {
     console.error('Chat socket closed unexpectedly');
 };
 
-document.querySelector('#set_clock_300').onclick = function(e) {
+window.onload = function(e) {
     setTimeout(setInterval(function(){
         chatSocket.send(JSON.stringify({'message': 'enviado request'}));
     }, 1000), 10000);
-    let user_code = location.href.split('/')[-1]
-    $.get("/user/" + user_code + "/OK", function(response) {
-        alert(response);
-    });
-};
-
-document.querySelector('#set_clock_1000').onclick = function(e) {
-    setTimeout(setInterval(function(){
-        chatSocket.send(JSON.stringify({'message': 'enviado request'}));
-    }, 1000), 60000);
-    let user_code = location.href.split('/')[-1]
-    $.get("/user/" + user_code + "/OK", function(response) {
-        alert(response);
-    });
 };
