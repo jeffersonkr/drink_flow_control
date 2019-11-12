8a572f73c9285d20b00a9f334b9b7f736fed9f87
Jefferson <jeffersonkr@hotmail.com>
2019-11-12 15:53:03 -0300

    fix file name

---
+++ core/consumers.py
@@ -0,0 +1,55 @@
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+import subprocess
+import paho.mqtt.subscribe as subscribe
+
+class MonitoringConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.mac_address = self.scope['url_route']['kwargs']['mac_address']
+        self.monitoring_group_name = 'monitoring_%s' % "-".join(self.mac_address.split(':'))
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.monitoring_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.monitoring_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        hostname = "moquitto"
+        msg = subscribe.simple(
+            topics=f'monitoramento/{self.mac_address}/',
+            retained=False, 
+            hostname=hostname,
+            port=1883, 
+            keepalive=60, 
+            auth={
+                'username': 'iot-autodoc',
+                'password': 'IOTautodoc19!'
+                }
+            )
+        await self.channel_layer.group_send(
+            self.monitoring_group_name,
+            {
+                'type': 'monitoring_message',
+                'message': msg.payload.decode('utf-8')
+            }
+        )
+
+    # Receive message from room group
+    async def monitoring_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
