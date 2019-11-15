0308eb1b1c453c2b1f76303786a997bdbf0792d9
jefferson kwak <jeffersonkr@hotmail.com>
2019-11-15 17:34:11 -0300

    adjust to flow per second and acumulative view

--- core/static/js/get_monitoring.js
+++ core/static/js/get_monitoring.js
@@ -3,8 +3,15 @@ var chatSocket = new WebSocket(
 
 chatSocket.onmessage = function(e) {
     var data = JSON.parse(e.data);
-    var message = JSON.parse(data['message']);
-    document.querySelector('#qtd_liquido').textContent = message;
+    var flowPerMin = JSON.parse(data['message']);
+    var flowPerSeconds = parseFloat(flowPerMin)/60;
+    var last_value;
+    if($('#qtd_liquidovar last_value').text != ""){
+        last_value = parseFloat($('#qtd_liquido').text);
+    } else {
+        last_value = 0.0;
+    }
+    document.querySelector('#qtd_liquido').textContent = (last_value + flowPerSeconds).toString();
     //document.querySelector('#qtd_faltante').textContent = total - qtd_liquido
     //document.querySelector('#qtd_total_diario').textContent = total
 };
