fbdcc07159da6eaf360a522ce0de2a1eabc8ef51
jefferson kwak <jeffersonkr@hotmail.com>
2019-11-16 19:50:50 -0300

    changed name to choose timer

---
+++ core/static/js/choose_timer.js
@@ -0,0 +1,9 @@
+document.querySelector('#set_clock_300').onclick = function(e){
+    let url = location.href;
+    location.replace(url + '/300');
+};
+
+document.querySelector('#set_clock_1000').onclick = function(e){
+    let url = location.href;
+    location.replace(url + '/1000');
+};
