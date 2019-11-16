61fb66b669f20c6f43a9c899066343ffbff6e7ba
jefferson kwak <jeffersonkr@hotmail.com>
2019-11-16 19:51:14 -0300

    add new timer test

--- core/static/js/timer.js
+++ core/static/js/timer.js
@@ -1,9 +1,2 @@
-document.querySelector('#set_clock_300').onclick = function(e){
-    let url = location.href;
-    location.replace(url + '/300');
-};
-
-document.querySelector('#set_clock_1000').onclick = function(e){
-    let url = location.href;
-    location.replace(url + '/1000');
-};
+var timer = (window.location.href).split("/")[-1];
+console.log(timer);
