32f866ca8a2d0745fc86687309366158e6d3eafe
jefferson kwak <jeffersonkr@hotmail.com>
2019-11-16 20:30:07 -0300

    fixing urlpatterns was missing comma

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('user/<int:user_id>', user, name='user'),
     path('cadastro/<str:code_number>', cadastro, name='cadastro'),
     path('edit/<str:code_number>', edit, name='edit'),
-    path('user/<int:user_id>/<int:qtd_water>', start_monitoring, name='monitoring')
-    path('user/<int:user_id>/<str:faltante>', close_solenoid, name='close-solenoid')
+    path('user/<int:user_id>/<int:qtd_water>', start_monitoring, name='monitoring'),
+    path('user/<int:user_id>/<str:faltante>', close_solenoid, name='close_solenoid'),
 ]
