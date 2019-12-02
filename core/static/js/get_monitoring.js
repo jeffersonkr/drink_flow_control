cf51c4d058b7fc5ea128ca95afdbf4b9c286b34f
jefferson kwak <jeffersonkr@hotmail.com>
2019-12-02 20:39:54 -0300

    fix if conditional

--- core/static/js/get_monitoring.js
+++ core/static/js/get_monitoring.js
@@ -19,7 +19,7 @@ chatSocket.onmessage = function(e) {
     } else {
         document.querySelector('#qtd_faltante').textContent = (parseFloat($('#qtd_faltante').text()) - MilliPerSeconds.toFixed(2))
         if(timer > parseFloat($('#qtd_liquido').text())){
-            if(timer > MilliPerSeconds*2 + parseFloat($('#qtd_liquido').text())){
+            if(timer <= MilliPerSeconds*2 + parseFloat($('#qtd_liquido').text())){
                 var site = location.href.split("/");
                 var faltante = $('#qtd_faltante').text();
                 site.pop();
